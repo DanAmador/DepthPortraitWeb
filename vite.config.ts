1e01140289161dd884f3f070e5ed3fc64817e141
Dan <perez@virtonomy.io>
2024-01-18 23:38:17 +0100

    wtf

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import {copy} from 'vite-plugin-copy';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/DepthPortraitWeb/' : '/',
   plugins: [
-    react(),
-    copy({
-      targets: [
-        { src: 'public/*', dest: 'assets' },
-      ],
-      hook: 'writeBundle', // or 'closeBundle' depending on your needs
-    }),
     // ... other plugins
   ],
 });

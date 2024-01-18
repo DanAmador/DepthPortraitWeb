16ebf943f5e1b29add3e5ba22204ed0a652b179e
Dan <perez@virtonomy.io>
2024-01-19 00:20:14 +0100

    Updates

--- src/custom.d.ts
+++
@@ -1,5 +0,0 @@
-// custom.d.ts
-declare module '*.glb' {
-    const content: any;
-    export default content;
-  }
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/DepthPortraitWeb/' : '/',
-  plugins: [
-    // ... other plugins
-  ],
-});

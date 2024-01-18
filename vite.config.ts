fda394ca35cc28f9263b86e381453fce0ba97316
Dan <perez@virtonomy.io>
2024-01-19 00:03:11 +0100

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/DepthPortraitWeb' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/DepthPortraitWeb/' : '/',
   plugins: [
     // ... other plugins
   ],
